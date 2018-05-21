d5e5c621e8d11eb777b3bbbaf58299451372fd19
121eddie <fedcba@sfr.fr>
2018-05-21 07:56:09 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,28 +4,44 @@ self.addEventListener('install', event => {//lors du chargement de index.html
   console.log("ServiceWorker installé");
 });
 
-self.addEventListener('activate', async function(event) {
+// self.addEventListener('activate', async function(event) {
+	// self.clients.claim();//le nouveau service worker prend le contrôle de toutes les pages ouvertes de l'appli web progressive
+	// console.log("ServiceWorker activé");
+	// if (navigator.onLine){
+		// var oReq = new Request('./version.txt?date='+new Date().getTime(), {method:'get'});
+		// var response=await fetch(oReq);
+		// var result=await response.text();
+		// console.log('Nouvelle version du cache: '+result);//a distance: [object promise]
+		// cache= await caches.open(result);
+		// console.log('cache:'+cache);
+		// for (var i=0;i<caches.length;i++){
+			// if (caches[i]!=cache){
+				// caches[i].delete();
+			// }
+		// }
+		// // return (cache.addAll(['/index.html','/styles.css','/traitement.js']));//ne traite que la promise
+		// try{await cache.addAll(['/index.html','/styles.css','/traitement.js']);//en localhost: TypeError: Request failed
+		// }catch (err){
+			// console.log(err);
+		// }//tout sauf version.txt //pas de résultat	
+	// }
+// });
+self.addEventListener('activate', event => {
 	self.clients.claim();//le nouveau service worker prend le contrôle de toutes les pages ouvertes de l'appli web progressive
 	console.log("ServiceWorker activé");
-	if (navigator.onLine){
-		var oReq = new Request('./version.txt?date='+new Date().getTime(), {method:'get'});
-		var response=await fetch(oReq);
-		var result=await response.text();
-		console.log('Nouvelle version du cache: '+result);//a distance: [object promise]
-		cache= await caches.open(result);
-		console.log('cache:'+cache);
-		for (var i=0;i<caches.length;i++){
-			if (caches[i]!=cache){
-				caches[i].delete();
-			}
-		}
-		// return (cache.addAll(['/index.html','/styles.css','/traitement.js']));//ne traite que la promise
-		try{await cache.addAll(['/index.html','/styles.css','/traitement.js']);//en localhost: TypeError: Request failed
-		}catch (err){
-			console.log(err);
-		}//tout sauf version.txt //pas de résultat	
-	}
+		if (navigator.onLine){
+					var oReq = new Request('./version.txt?date='+new Date().getTime(), {method:'get'});
+			event.waitUntil(fetch(oReq))
+    .then(response=>return caches.open(response)).then(cache=>return cache.keys()).then(keys => Promise.all(
+      keys.map(key => {
+          return caches.delete(key);
+        }
+    ))).then(() => {
+      console.log('V2 now ready to handle fetches!');
+    })
+  );
 });
+
 
 //ServiceListener standard qui met en cache tout ce qui passe
 //prévoit un chargement online avec mise en cache si quelquechose manque
