dd54e9c6d7ac63c75715221f02f0cde095637c67
121eddie <fedcba@sfr.fr>
2018-05-21 22:52:47 +0200

    Add files via upload

--- traitement.js
+++ traitement.js
@@ -105,14 +105,3 @@ function sms(){
 	window.location.href=sms;
 	console.log("SMS ouvert dans l'application SMS");
 }
-
-// var enregistrer = document.getElementById("enregistrer");
-// enregistrer.onclick=function (){
-function enregistrer(){
-	console.log('enregistrer');
-	fileSystem.chooseEntry({'type':'saveFile','suggestedName':titre,'accepts':'txt'},file=>{
-		file.createWriter(writer=>{//succes de la creation du writer
-			return writer.write(message);
-		},console.log('Echec de la création du writer'));
-	});
-}
